3bc39ee795408368671cb9445fa59779dfe9c110
Deserv0502 <1347475525@qq.com>
2020-12-17 14:16:55 +0800

    1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
+//import ReactDom from 'react-dom'
+//import PropTypes from 'prop-types'
+//import axios from 'axios'
+//import { NavLink,Switch,Route,Redirect } from 'react-router-dom'
 import './App.css';
+import './antd.min.css'
+// import '../public/font_w5ue8o15udk/iconfont.css'
+import { Layout, Menu, Breadcrumb } from 'antd';
+import ToDolist from './component/todoList/ToDolist'
+const { Header, Content, Footer } = Layout;
+export default class App extends Component {
+  render() {
+    return (
+      <div style={{height:'100%'}}>
+        <Layout className="layout">
+          <Header>
+            <div className="logo" />
+            <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+              <Menu.Item key="1">ToDoList</Menu.Item>
+              <Menu.Item key="2">nav 2</Menu.Item>
+              <Menu.Item key="3">nav 3</Menu.Item>
+            </Menu>
+          </Header>
+          <Content style={{ padding: '0 50px' }}>
+            <Breadcrumb style={{ margin: '16px 0' }}>
+              <Breadcrumb.Item>Home</Breadcrumb.Item>
+              <Breadcrumb.Item>List</Breadcrumb.Item>
+              <Breadcrumb.Item>App</Breadcrumb.Item>
+            </Breadcrumb>
+            <div className="site-layout-content" style={{height:'100%'}}>
+            <ToDolist />
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}></Footer>
+        </Layout>
+      </div>
+    )
+  }
 }
-
-export default App;
---
+++ src/component/todoList/ToDolist.jsx
@@ -0,0 +1,167 @@
+import React, { Component } from 'react'
+//import ReactDom from 'react-dom'
+//import PropTypes from 'prop-types'
+//import axios from 'axios'
+//import { NavLink,Switch,Route,Redirect } from 'react-router-dom'
+import './ToDolist.css'
+
+export default class ToDolist extends Component {
+    state = {
+        unFinishedList: [
+            { text: "111", checked: false },
+            { text: "2222", checked: false }
+        ],
+        FinishedList: [
+            { text: "333", checked: true },
+        ],
+        value:'',
+    }
+    checked = (event, index) => {
+        let { unFinishedList } = this.state;
+        let { FinishedList } = this.state;
+        if (event.target.checked) {
+            unFinishedList[index].checked = true;
+            FinishedList.push(unFinishedList[index])
+            unFinishedList.splice(index, 1);
+            this.setState({ unFinishedList });
+            event.target.checked = false;
+        } 
+    }
+    delUnFinList = (index) => {
+        let { unFinishedList } = this.state;
+        unFinishedList.splice(index, 1);
+        this.setState({ unFinishedList });
+    }
+    delFinList = (index) => {
+        let { FinishedList } = this.state;
+        FinishedList.splice(index, 1);
+        this.setState({ FinishedList });
+    }
+    todoClickSub = () => {
+        let { unFinishedList } = this.state;
+        let {value} = this.state;
+        let temp = { text: value, checked: false };
+        unFinishedList.push(temp);
+        this.setState({ unFinishedList });
+        value = '';
+        this.setState({value})
+    }
+    handleEnter = (event) => {
+        if (event.charCode === 13) {
+            this.todoClickSub();
+        }
+    }
+    SubmitText = (event) =>{
+        this.setState({value:event.target.value})
+    }
+    todoRetuen =(index) =>{
+        let { unFinishedList } = this.state;
+        let { FinishedList } = this.state;
+        unFinishedList.push(FinishedList[index])
+        FinishedList.splice(index, 1);
+        this.setState({ FinishedList });
+    } 
+    render() {
+        let {unFinishedList} = this.state;
+        let {FinishedList} = this.state;
+        let {value} = this.state;
+        return (
+            <>
+             <h1 className='todoH1'>ToDoList</h1>
+            <div className='todoContan'>
+                <div className='todoSub'>
+                    <input type="text"
+                        onKeyPress={(event) => this.handleEnter(event)}
+                        value={value}
+                        onChange={(event)=>this.SubmitText(event)}
+                    />
+                    <button
+                        onClick={this.todoClickSub}
+                    >
+                        添加
+                        <span className='iconfont'>&#xe608;</span>
+                    </button>
+                </div>
+                <p className='todoTitle'>
+                    <span className='todoIcon iconfont'>&#xe606;</span>
+                    今日待办
+                    <span className='todoNum'>{unFinishedList.length}</span>
+                    条
+                </p>
+                <ul className='unFinished' >
+                    {
+                        unFinishedList.map((unFList, index) => {
+                            return (
+                                <li key={index}
+                               className='todoLi'
+                                >
+                                    <input type="checkbox" 
+                                        onChange={(event) => this.checked(event, index)
+                                        }
+                                        id="input-1"
+                                    />
+                                    <label
+                                    htmlFor="input-1"
+                                    className='label-check'
+                                    >  
+                                    </label>
+                                    <span className='todoList'>
+                                    {unFList.text}
+                                    </span>
+                                    <span className='todoDelList iconfont'
+                                        onClick={() => this.delUnFinList(index)}
+                                    >
+                                   &#xe604;
+                                    </span>
+                                </li>
+                            )
+                        }
+                        )}
+                </ul>
+                <p className='todoTitle'>
+                <span className='todoIcon iconfont'>&#xe605;</span>
+                    已经完成
+                    <span className='todoNum'>
+                {FinishedList.length}
+                </span>
+                条
+            </p>
+                <ul className='Finished'>
+                    {
+                        FinishedList.map((FList, index) => {
+                            return (
+                                <li key={index}
+                                className='todoLi'
+                                >
+                                <input type="checkbox" 
+                                       readOnly
+                                        checked={FList.checked}
+                                        id="input-2"
+                                    />
+                                    <label htmlFor="input-2"
+                                    className='label-check label-check2'
+                                    >
+                                    </label>
+                                     <span className='todoList'>
+                                     {FList.text}
+                                     </span>
+                                     <span className='todoDelList iconfont'
+                                        onClick={() => this.delFinList(index)}
+                                    >
+                                   &#xe604;
+                                    </span>
+                                    <span className='todoReturn iconfont'
+                                    onClick={(event) => this.todoRetuen(event, index)}
+                                    >
+                                    &#xe61d;
+                                    </span>
+                                </li>
+                            )
+                        }
+                        )}
+                </ul>
+            </div>
+            </>
+        )
+    }
+}
